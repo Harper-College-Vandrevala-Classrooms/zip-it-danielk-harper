bfc3e1e3e4526fff8efd293f6488071be2bca0db
danielk-harper <d_kaeseberg1@mail.harpercollege.edu>
2024-10-13 22:33:06 -0500

    Project

--- src/test/java/com/csc/ZipperTest.java
+++ src/test/java/com/csc/ZipperTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.*;
+
 public class ZipperTest {
 
   Zipper zipper;
@@ -13,9 +15,56 @@ public class ZipperTest {
   void setUp() {
     zipper = new Zipper();
   }
-
+  
   @Test
-  void itWorks() {
-    assertEquals(true, true);
+  void noElements() {
+    List<Integer> a = List.of();
+    List<Integer> b = List.of();
+    
+    List<Integer> c = zipper.zip(a, b);
+    
+    assertEquals(c.size(), 0);
+  }
+  
+  @Test
+  void firstHasMore() {
+    List<Integer> a = List.of(1, 2, 4);
+    List<Integer> b = List.of();
+    
+    List<Integer> c = zipper.zip(a, b);
+    
+    assertEquals(c.size(), 3);
+    assertEquals(c.get(0), 1);
+    assertEquals(c.get(1), 2);
+    assertEquals(c.get(2), 4);
+  }
+  
+  @Test
+  void secondHasMore() {
+    List<Integer> a = List.of();
+    List<Integer> b = List.of(1, 2, 4);
+    
+    List<Integer> c = zipper.zip(a, b);
+    
+    assertEquals(c.size(), 3);
+    assertEquals(c.get(0), 1);
+    assertEquals(c.get(1), 2);
+    assertEquals(c.get(2), 4);
+  }
+  
+  @Test
+  void testInterleaving() {
+    List<Integer> a = List.of(3, 7, 9);
+    List<Integer> b = List.of(1, 2, 4);
+    
+    List<Integer> c = zipper.zip(a, b);
+    
+    assertEquals(c.size(), 6);
+    assertEquals(c.get(0), 3);
+    assertEquals(c.get(1), 1);
+    assertEquals(c.get(2), 7);
+    assertEquals(c.get(3), 2);
+    assertEquals(c.get(4), 9);
+    assertEquals(c.get(5), 4);
   }
 }
