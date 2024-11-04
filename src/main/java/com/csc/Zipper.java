735f8b323beb1b0f74dc5d28a206db6d08753afd
danielk-harper <d_kaeseberg1@mail.harpercollege.edu>
2024-11-03 23:14:50 -0600

    Portfolio

--- src/main/java/com/csc/Zipper.java
+++ src/main/java/com/csc/Zipper.java
@@ -1,6 +1,7 @@
 package com.csc;
 
 import java.util.*;
+import java.lang.*;
 
 public class Zipper {
   
@@ -23,15 +24,24 @@ public class Zipper {
     return out;
   }
   
+  public static <T> HashMap<String, T> hashmapify(List<String> strings, List<T> elements) {
+    if (strings.size() != elements.size()) throw new ArrayIndexOutOfBoundsException("strings and elements lists must be the same size.");
+    
+    HashMap<String, T> hashmap = new HashMap<String, T>();
+    
+    for(int i = 0; i < strings.size(); i++) {
+      hashmap.put(strings.get(i), elements.get(i));
+    }
+    
+    return hashmap;
+  }
+  
   public static void main(String[] args) {
-    List<Integer> nums1 = List.of(1, 3, 5, 7);
-    List<Integer> nums2 = List.of(2, 4, 6, 8);
-    List<Integer> mergedNumbers = zip(nums1, nums2);
-    System.out.println(mergedNumbers); // [1, 2, 3, 4, 5, 6, 7, 8]
+    List<String> colors = List.of("White", "Black", "Orange", "Pink");
+    List<Integer> nums = List.of(1, 3, 5, 7);
 
-    List<String> colors1 = List.of("Red", "Green", "Blue");
-    List<String> colors2 = List.of("White", "Black", "Orange", "Pink");
-    List<String> mergedWords = zip(colors1, colors2);
-    System.out.println(mergedWords); // [Red, White, Green, Black, Blue, Orange, Pink]
+    // This should create {"White" => 1, "Black" => 3, "Orange" => 5, "Pink" => 7}
+    HashMap<String, Integer> map = hashmapify(colors, nums);
+    System.out.println(map.get("White"));
   }
 }
